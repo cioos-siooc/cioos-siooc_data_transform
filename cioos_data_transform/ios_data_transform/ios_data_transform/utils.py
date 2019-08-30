72226fa53678d4cef5e4cd63416aa28ce0d93d8b
Pramod <thupakip@hotmail.com>
2019-08-30 15:22:36 -0700

    assign geocode to point
    read geojson file with named polygons and construct geocode

--- cioos_data_transform/ios_data_transform/ios_data_transform/utils.py
+++ cioos_data_transform/ios_data_transform/ios_data_transform/utils.py
@@ -1,3 +1,5 @@
+from shapely.geometry import Polygon, Point
+import json
 # general utility functions common to multiple classes
 
 def is_in(keywords, string):
@@ -34,3 +36,31 @@ def release_memory(outfile):
     for c in outfile.varlist:
         del c
     del outfile
+
+
+def read_geojson(filename):
+    # read shapefile in geojson format into Polygon object
+    # input geojson file
+    # output: Polygon object
+    with open(filename) as f:
+        data = json.load(f)
+    poly_dict = {}
+
+    for feature in data['features']:
+        if feature['geometry']['type'] == 'Polygon':
+            # print(feature['geometry']['coordinates'][0])
+            p = Polygon(feature['geometry']['coordinates'][0])
+            name = feature['properties']['name']
+            poly_dict[name] = p
+    return poly_dict
+
+def is_in_polygon(polygon, point):
+    # identify if point is inside polygon
+    return polygon.contains(point)
+
+def find_geographic_area(poly_dict, point):
+    name_str = ''
+    for key in poly_dict:
+        if is_in_polygon(poly_dict[key], point):
+            name_str = '{}{} '.format(name_str, key)
+    return name_str
