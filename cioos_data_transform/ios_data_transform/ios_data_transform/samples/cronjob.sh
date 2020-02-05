dea694450a1be929327f475ec70a002252a4b25a
pramod <thupakip@hotmail.com>
2020-02-05 12:04:52 -0800

    update cron script

--- cioos_data_transform/ios_data_transform/ios_data_transform/samples/cronjob.sh
+++ cioos_data_transform/ios_data_transform/ios_data_transform/samples/cronjob.sh
@@ -10,18 +10,18 @@ cd /home/ios/
 # run script to convert new ctd files
 python --version
 echo `which python`
-python ios_data_transform_script.py new ctd > ~/log.ctd 2>&1
-python ios_data_transform_script.py all mctd > ~/log.mctd 2>&1
-python ios_data_transform_script.py all bot > ~/log.bot 2>&1
+python ios_data_transform_script.py new ctd > ~/ctd.log 2>&1
+python ios_data_transform_script.py all mctd > ~/mctd.log 2>&1
+python ios_data_transform_script.py all bot > ~/bot.log 2>&1
 
 
 # set flag on ERDDAP to reload/rebuild dataset
 #touch /home/hakai/erddap-ios/bigParentDirectory/hardFlag/IOS_CTD_test
-wget -qO - --user cioos --password DeepWaters2  "https://pac-dev1.cioos.org/erddap/setDatasetFlag.txt?datasetID=IOS_CTD_Profiles&flagKey=5754e1b637451fcbd8b0fc6e3741bacd02743ecfb533533c5c3fe98bacb0208c" >> log.ctd
-wget -qO - --user cioos --password DeepWaters2  "https://pac-dev1.cioos.org/erddap/setDatasetFlag.txt?datasetID=IOS_CTD_Moorings&flagKey=56c5d942e8aac6da23086410e978d5ca12c0cd2f796066f8496d615db1fba042" >> log.mctd
-wget -qo - --user cioos --password DeepWaters2  "https://pac-dev1.cioos.org/erddap/setDatasetFlag.txt?datasetID=IOS_BOT_Profiles&flagKey=da740e78f9344e340c8625a1a98c5a64747add5fa9faa322b2ba8fdeebf7aec4" >> log.bot
+wget -qO - --user cioos --password ***  "https://pac-dev1.cioos.org/erddap/setDatasetFlag.txt?datasetID=IOS_CTD_Profiles&flagKey=5754e1b637451fcbd8b0fc6e3741bacd02743ecfb533533c5c3fe98bacb0208c" >> ctd.log
+wget -qO - --user cioos --password ***  "https://pac-dev1.cioos.org/erddap/setDatasetFlag.txt?datasetID=IOS_CTD_Moorings&flagKey=56c5d942e8aac6da23086410e978d5ca12c0cd2f796066f8496d615db1fba042" >> mctd.log
+wget -qo - --user cioos --password ***  "https://pac-dev1.cioos.org/erddap/setDatasetFlag.txt?datasetID=IOS_BOT_Profiles&flagKey=da740e78f9344e340c8625a1a98c5a64747add5fa9faa322b2ba8fdeebf7aec4" >> bot.log
 # mail someone with the results or with error message
-mail -s "CIOOS message(ctd)" pramod.thupaki@hakai.org < ~/log.ctd
-mail -s "CIOOS message(mctd)" pramod.thupaki@hakai.org < ~/log.mctd
-mail -s "CIOOS message(bot)" pramod.thupaki@hakai.org < ~/log.bot
+mail -s "CIOOS message(ctd)" pramod.thupaki@hakai.org < ~/ctd.log
+mail -s "CIOOS message(mctd)" pramod.thupaki@hakai.org < ~/mctd.log
+mail -s "CIOOS message(bot)" pramod.thupaki@hakai.org < ~/bot.log
 
