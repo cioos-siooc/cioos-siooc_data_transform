e90280f23d6b22e672e311e57e5f8a258f4f0966
pramod <pramod.thupaki@hakai.org>
2020-02-26 10:32:03 -0800

    make test.py os sensitive
    closes #9

--- cioos_data_transform/ios_data_transform/ios_data_transform/tests/test.py
+++ cioos_data_transform/ios_data_transform/ios_data_transform/tests/test.py
@@ -5,11 +5,20 @@ sys.path.insert(0, os.getcwd()+'/../../')
 import ios_data_transform as iod
 from glob import glob
 
+
+def fix_path(path):
+    # converts path from posix to nt if system is nt
+    # input is string with path in posix format '/' file sep
+    if os.name == 'nt':
+        path = os.path.sep.join(path.split('/'))
+    return path
+
+
 def convert_mctd_files(f, out_path):
     fdata = iod.MCtdFile(filename=f, debug=False)
     if fdata.import_data():
-        fdata.assign_geo_code('../tests/test_files/ios_polygons.geojson')
-        iod.write_mctd_ncfile(out_path+f.split('/')[-1]+'.nc', fdata)
+        fdata.assign_geo_code(fix_path('test_files/ios_polygons.geojson'))
+        iod.write_mctd_ncfile(fix_path(out_path+f.split('/')[-1]+'.nc'), fdata)
     else:
         print("Unable to import data from file", fdata.filename)
 
@@ -19,8 +28,8 @@ def convert_bot_files(f, out_path):
     print(fdata.filename)
     if fdata.import_data():
         # print(fdata.data)
-        fdata.assign_geo_code('../tests/test_files/ios_polygons.geojson')
-        iod.write_ctd_ncfile(out_path+f.split('/')[-1]+'.nc', fdata)
+        fdata.assign_geo_code(fix_path('test_files/ios_polygons.geojson'))
+        iod.write_ctd_ncfile(fix_path(out_path+f.split('/')[-1]+'.nc'), fdata)
     else:
         print("Unable to import data from file", fdata.filename)
 
@@ -30,19 +39,19 @@ def convert_ctd_files(f, out_path):
     print(fdata.filename)
     if fdata.import_data():
         # print(fdata.data)
-        fdata.assign_geo_code('../tests/test_files/ios_polygons.geojson')
-        iod.write_ctd_ncfile(out_path+f.split('/')[-1]+'.nc', fdata)
+        fdata.assign_geo_code(fix_path('test_files/ios_polygons.geojson'))
+        iod.write_ctd_ncfile(fix_path(out_path+f.split('/')[-1]+'.nc'), fdata)
     else:
         print("Unable to import data from file", fdata.filename)
 
 
-for fn in glob('./test_files/ctd_mooring/*.*', recursive=True):
-    convert_mctd_files(f=fn, out_path='./temp/')
+for fn in glob(fix_path('./test_files/ctd_mooring/*.*'), recursive=True):
+    convert_mctd_files(f=fn, out_path=fix_path('./temp/'))
 
-for fn in glob('./test_files/ctd_profile/*.*', recursive=True):
-    convert_ctd_files(f=fn, out_path='./temp/')
+for fn in glob(fix_path('./test_files/ctd_profile/*.*'), recursive=True):
+    convert_ctd_files(f=fn, out_path=fix_path('./temp/'))
 
-for fn in glob('./test_files/bot/*.*', recursive=True):
-    convert_bot_files(f=fn, out_path='./temp/')
+for fn in glob(fix_path('./test_files/bot/*.*'), recursive=True):
+    convert_bot_files(f=fn, out_path=fix_path('./temp/'))
 
 # print(iod.utils.compare_file_list(['a.bot', 'c.bkas.asd'], ['a.nc', 'b.nc', 'c.nc', 'd.nc']))
