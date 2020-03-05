fd98d52471eea86914c8cbab5c63096c1b610ec4
Jessy Barrette <jessy.barrette@hakai.org>
2020-03-05 15:24:45 -0800

    add access to add_standard_variables in the package

--- cioos_data_transform/ios_data_transform/ios_data_transform/__init__.py
+++ cioos_data_transform/ios_data_transform/ios_data_transform/__init__.py
@@ -2,3 +2,4 @@ from .ObsFile import CtdFile, MCtdFile, BotFile
 from .write_ctd_ncfile import write_ctd_ncfile
 from .write_mctd_ncfile import write_mctd_ncfile
 from .utils.utils import import_env_variables, is_in, file_mod_time, read_geojson, find_geographic_area, compare_file_list
+from .utils.utilsNC import add_standard_variables
