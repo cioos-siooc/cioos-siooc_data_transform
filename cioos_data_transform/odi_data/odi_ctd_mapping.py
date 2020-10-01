95dcca0901a95ab17b0da840179791e4f3ddb5df
jeffcullis <jcullis@dal.ca>
2020-10-01 12:35:42 -0300

    Add scripts to parse ODI CTD netcdf headers and build metadata YAML file.

--- cioos_data_transform/odi_data/odi_ctd_mapping.py
+++ cioos_data_transform/odi_data/odi_ctd_mapping.py
@@ -1,6 +1,8 @@
 
 import uuid
+import yaml
 from netCDF4 import Dataset
+
 
 class OdiCtdMapping(object):
 
@@ -12,7 +14,12 @@ class OdiCtdMapping(object):
 
     def create_dict(self):
         self.collect_standard_names()
-        self.create_odi_dict()
+        self.add_metadata(),
+        self.add_spatial(),
+        self.add_identification(),
+        self.add_contact(),
+        self.add_distribution(),
+        self.add_platform(),
 
     def collect_standard_names(self):
         """ Get all the standard names for the data variables into a list. """
@@ -20,77 +27,63 @@ class OdiCtdMapping(object):
             if hasattr(nc_var, 'standard_name'):
                 self.standard_names.append(nc_var.standard_name)
 
-    def create_odi_dict(self):
-        self.odi_dict = {
-            'metadata': self.create_metadata(),
-            'spatial': self.create_spatial(),
-            'identification': self.create_identification(),
-            'contact': self.create_contact(),
-            'distribution': self.create_distribution(),
-            'platform': self.create_platform(),
-        }
+    def write_yaml(self, yaml_filename):
+        with open(yaml_filename, 'w') as yaml_file:
+            yaml.dump(self.odi_dict, yaml_file, default_flow_style=False, sort_keys=False)
 
-    def write_yaml(self):
-        pass
-
-    def create_metadata(self):
-        metadata = {
+    def add_metadata(self):
+        self.odi_dict['metadata'] = {
             'naming_authority': {
-                'en': 'ca-gc-dfo-mpo',
-                'fr': 'ca-gc-dfo-mpo',
+                'en': 'ca.gc.dfo-mpo',
+                'fr': 'ca.gc.dfo-mpo',
             },
-            'identifier': uuid.uuid4(),
+            'identifier': str(uuid.uuid1()),
             'language': 'en',
             'use_constraints': {
-                'limitations': 'licence',
+                'limitations': 'limitations',
                 'licence': {
                     'title': 'Open Government Licence - Canada',
-                    'code': '',
+                    'code': 'open-government-licence-canada',
                     'url': 'https://open.canada.ca/en/open-government-licence-canada',
                 }
             },
-            'comment': '',
             'history': {
                 'en': self.nc['history'],
             }
         }
-        return metadata
 
-    def create_spatial(self):
-        vertical_crs = ''
-        if self.nc['geospatial_vertical_positive'] == 'down':
-            # Positive depth from surface: https://epsg.io/5831
-            vertical_crs = 'EPSG:5831'
-        elif self.nc['geospatial_vertical_positive'] == 'up':
-            # Positive height from sea floor https://epsg.io/5829
-            vertical_crs = 'EPSG:5829'
-        spatial = {
+    def add_spatial(self):
+        self.odi_dict['spatial'] = {
             'bbox': [
-                self.nc['geospatial_lat_min'],
-                self.nc['geospatial_lon_min'],
-                self.nc['geospatial_lat_max'],
-                self.nc['geospatial_lon_max']
+                str(self.nc['geospatial_lat_min']),
+                str(self.nc['geospatial_lon_min']),
+                str(self.nc['geospatial_lat_max']),
+                str(self.nc['geospatial_lon_max'])
             ],
             'vertical': [
-                self.nc['geospatial_vertical_min'],
-                self.nc['geospatial_vertical_max']
+                str(self.nc['geospatial_vertical_min']),
+                str(self.nc['geospatial_vertical_max'])
             ],
-            'vertical_crs': vertical_crs
+            'vertical_positive': self.nc['geospatial_vertical_positive']
         }
-        return spatial
 
-    def create_identification(self):
-        identification = {
+    def add_identification(self):
+        title = (
+            f"{self.nc['data_type']}_{self.nc['cruise_number']}_"
+            f"{self.nc['mooring_number']}_{self.nc['serial_number']}_"
+            f"{round(float(self.nc['sampling_interval']))}"
+        )
+        self.odi_dict['identification'] = {
             'title': {
-                'en': f"{self.nc['platform']} - {self.nc['data_type']}",
-                'fr': f"{self.nc['platform']} - {self.nc['data_type']}"
+                'en': title,
+                'fr': title,
             },
             'abstract': {
                 'en': 'Abstract',
                 'fr': 'Abstrait'
             },
             'dates': {
-                'created': self.nc['NCO']
+                'creation': self.nc['NCO']
             },
             'keywords': {
                 'default': {
@@ -98,7 +91,7 @@ class OdiCtdMapping(object):
                     'fr': self.standard_names
                 },
                 # TODO: create mapping between DFO standard names and CIOOS EOVs
-                'eov': ['seaWaterTemperature', 'salinity', 'oxygen']
+                'eov': ['subSurfaceTemperature', 'subSurfaceSalinity', 'oxygen']
             },
             'temporal_begin': self.nc['time_coverage_start'],
             'temporal_end': self.nc['time_coverage_end'],
@@ -106,18 +99,17 @@ class OdiCtdMapping(object):
             # TODO: Does nc['sampling_interval'] go here or in instrument?
             'status': 'completed',
         }
-        return identification
 
-    def create_contact(self):
-        contact = [
+    def add_contact(self):
+        self.odi_dict['contact'] = [
             {
                 'roles': ['owner'],
                 'organization': {
                     'name': self.nc['institution'],
                     'url': 'https://www.bio.gc.ca/index-en.php',
                     'address': (
-                        'Bedford Institute of Oceanography'
-                        'P.O. Box: 1006'
+                        'Bedford Institute of Oceanography, '
+                        'P.O. Box: 1006, '
                         'B2Y 4A2'
                     ),
                     'city': 'Dartmouth, NS',
@@ -151,34 +143,33 @@ class OdiCtdMapping(object):
                 'roles': ['custodian'],
                 'individual': {
                     'name': 'Diana Cardoso',
-                    'email': ''
+                    'email': 'Diana.Cardoso@dfo-mpo.gc.ca'
                 },
             },
         ]
-        return contact
 
-    def create_distribution(self):
-        distribution = [
+    def add_distribution(self):
+        self.odi_dict['distribution'] = [
             {
                 # TODO: put ERDDAP URL for dataset here
-                'url': ''
+                'url': 'https://inter-j01.dfo-mpo.gc.ca/odiqry/index-e.html'
                 # TODO: 'name' and 'description' elements also mandatory here...
             }
         ]
-        return distribution
 
-    def create_platform(self):
+    def add_platform(self):
         platform_name = (
             f"{self.nc['platform']} ({self.nc['cruise_number']}) "
-            f"mooring {self.nc['mooring_number']})"
+            f"mooring {self.nc['mooring_number']}"
         )
-        platform = {
+        (inst_model, inst_version) = self.nc['model'].split(' ')
+        self.odi_dict['platform'] = {
             # Note: platform->name maps to CI_Citation title
             'name': platform_name,
             # platform->id maps to identifier->code (NERC C17 recommended)
             # 18HU = NERC C17 ID for DFO CCGS Hudson vessel
             # http://vocab.nerc.ac.uk/collection/C17/current/18HU/
-            'id': '18HU',
+            'id': 'http://vocab.nerc.ac.uk/collection/C17/current/18HU/',
             'authority': 'International Council for the Exploration of the Sea (ICES)',
             # TODO: role should be owner or resourceProvider or publisher?
             'role': 'owner',
@@ -190,9 +181,21 @@ class OdiCtdMapping(object):
             'description': (
                 f"{platform_name}, NERC L06 keywords: (research vessel, mooring),"
                 f"CCGS Hudson"
-            )
+            ),
+            'instruments': [
+                {
+                    # TODO: parse and lookup NERC L22 ID from inst_model
+                    'id': self.nc['serial_number'],
+                    'type': { 'en': 'http://vocab.nerc.ac.uk/collection/L22/current/TOOL1393/', },
+                    'description': {
+                        'en': ('A series of high accuracy conductivity and temperature recorders '
+                            'with integrated pressure sensors designed for deployment on moorings.')
+                    },
+                    'manufacturer': 'Sea-Bird',
+                    'version': inst_version,
+                }
+            ]
         }
-        return platform
 
 
 
