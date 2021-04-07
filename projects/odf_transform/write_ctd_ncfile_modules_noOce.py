42e6fbd70a069567852175e9e7d46bc4216a580c
Pramod Thupaki <thupakip@hotmail.com>
2021-04-07 15:32:16 -0700

    re-organise files after merge

--- projects/odf_transform/write_ctd_ncfile_modules_noOce.py
+++ projects/odf_transform/write_ctd_ncfile_modules_noOce.py
@@ -6,8 +6,8 @@ import re
 import shutil
 
 import xarray as xr
-import cioos_data_transform.utils.odf as odf
-import cioos_data_transform.utils.xarray as xarray_methods
+import utils.odf as odf
+import cioos_data_transform.utils.xarray_methods as xarray_methods
 from cioos_data_transform.utils.utils import fix_path
 from cioos_data_transform.utils.utils import get_geo_code, read_geojson
 import datetime as dt
@@ -20,7 +20,7 @@ def read_config(config_file):
         config = json.load(fid)
 
         # Read Vocabulary file
-        for vocab_file in config['vocabularyFileList']:
+        for vocab_file in config["vocabularyFileList"]:
             config.update({"vocabulary": {}})
             with open(vocab_file) as fid:
                 vocab = json.load(fid)
@@ -29,23 +29,25 @@ def read_config(config_file):
         return config
 
 
-def write_ctd_ncfile(odf_path,
-                     output_path,
-                     config=None,
-                     variable_header_section='PARAMETER_HEADER',
-                     original_prefix_var_attribute='original_',
-                     variable_name_attribute='CODE'):
+def write_ctd_ncfile(
+    odf_path,
+    output_path,
+    config=None,
+    variable_header_section="PARAMETER_HEADER",
+    original_prefix_var_attribute="original_",
+    variable_name_attribute="CODE",
+):
 
-    if not os.path.isdir(config['TEST_FILES_OUTPUT']):
-        os.mkdir(config['TEST_FILES_OUTPUT'])
+    if not os.path.isdir(config["TEST_FILES_OUTPUT"]):
+        os.mkdir(config["TEST_FILES_OUTPUT"])
 
     print(os.path.split(odf_path)[-1])
     # Parse the ODF file with the CIOOS python parsing tool
     metadata, raw_data = odf.read(odf_path)
 
     # Add the file name as variable
-    raw_data['original_file'] = os.path.split(odf_path)[-1]
-    raw_data = raw_data.set_index('original_file')
+    raw_data["original_file"] = os.path.split(odf_path)[-1]
+    raw_data = raw_data.set_index("original_file")
     original_variables = raw_data.columns
 
     # create unique ID for each profile
@@ -58,38 +60,52 @@ def write_ctd_ncfile(odf_path,
 
     # #### Write global attributes ####
     # From the config file
-    ds.attrs.update(config['global_attributes'])
+    ds.attrs.update(config["global_attributes"])
 
     # # From ODF header
-    ds = xarray_methods.add_variables_from_dict(ds,
-                                                config['global_attributes_from_header'],
-                                                None,
-                                                metadata,
-                                                global_attribute=True)
+    ds = xarray_methods.add_variables_from_dict(
+        ds,
+        config["global_attributes_from_header"],
+        None,
+        metadata,
+        global_attribute=True,
+    )
     # Add all the original header as a json string
-    ds.attrs['header'] = json.dumps(metadata, ensure_ascii=False, indent=False)
+    ds.attrs["header"] = json.dumps(metadata, ensure_ascii=False, indent=False)
 
     # Add Variable from Global Attributes
-    if 'variables_from_header' in config:
-        ds = xarray_methods.add_variables_from_dict(ds, config['variables_from_header'],
-                                                    'original_file', dictionary=metadata)
-        metadata_variable_list = list(config['variables_from_header'].keys())
+    if "variables_from_header" in config:
+        ds = xarray_methods.add_variables_from_dict(
+            ds,
+            config["variables_from_header"],
+            "original_file",
+            dictionary=metadata,
+        )
+        metadata_variable_list = list(config["variables_from_header"].keys())
     else:
         metadata_variable_list = []
 
     # geographic_area
-    ds['geographic_area'] = get_geo_code([ds['longitude'], ds['latitude']], config["polygons_dict"])
+    ds["geographic_area"] = get_geo_code(
+        [ds["longitude"], ds["latitude"]], config["polygons_dict"]
+    )
 
     # Add Variable Attributes
     # Convert metadata variable attributes list to dictionary
-    var_attributes = {var[variable_name_attribute]: {original_prefix_var_attribute + att: value
-                                                     for att, value in var.items()}
-                      for var in metadata[variable_header_section]}
+    var_attributes = {
+        var[variable_name_attribute]: {
+            original_prefix_var_attribute + att: value
+            for att, value in var.items()
+        }
+        for var in metadata[variable_header_section]
+    }
 
     # Add Vocabulary attributes
-    var_attributes = odf.define_odf_variable_attributes(var_attributes,
-                                                        organizations=config['organisationVocabulary'],
-                                                        vocabulary=config['vocabulary'])
+    var_attributes = odf.define_odf_variable_attributes(
+        var_attributes,
+        organizations=config["organisationVocabulary"],
+        vocabulary=config["vocabulary"],
+    )
 
     # Add variable attributes to ds variables
     for var, attrs in var_attributes.items():
@@ -97,32 +113,42 @@ def write_ctd_ncfile(odf_path,
 
         # Keep the original long_name and units for now, except if it doesn't exist or
         # None or was populated already (flags)
-        if 'long_name' not in ds[var].attrs and \
-                ds[var].attrs.get('original_NAME'):
-            ds[var].attrs['long_name'] = ds[var].attrs.get('original_NAME')
-        if 'units' not in ds[var].attrs and \
-                ds[var].attrs.get('original_UNITS') and \
-                ds[var].attrs['original_UNITS'] not in ['nan', '(none)', 'none']:
-            ds[var].attrs['units'] = ds[var].attrs.get('original_UNITS')
+        if "long_name" not in ds[var].attrs and ds[var].attrs.get(
+            "original_NAME"
+        ):
+            ds[var].attrs["long_name"] = ds[var].attrs.get("original_NAME")
+        if (
+            "units" not in ds[var].attrs
+            and ds[var].attrs.get("original_UNITS")
+            and ds[var].attrs["original_UNITS"]
+            not in ["nan", "(none)", "none"]
+        ):
+            ds[var].attrs["units"] = ds[var].attrs.get("original_UNITS")
 
     # Generate Variables from Config
-    if 'variables' in config:
-        ds = xarray_methods.add_variables_from_dict(ds, config['variables'], 'original_file', dictionary=metadata)
+    if "variables" in config:
+        ds = xarray_methods.add_variables_from_dict(
+            ds, config["variables"], "original_file", dictionary=metadata
+        )
 
     # Generate extra variables (BODC, Derived)
     # TODO This need to be added in the near future to make the data output fully usable
 
     # Add geospatial and geometry related global attributes
-    ds = xarray_methods.get_spatial_coverage_attributes(ds)  # Just add spatial/time range as attributes
-    ds = xarray_methods.convert_variables_to_erddap_format(ds)  # Add encoding information to netcdf and convert strings
+    ds = xarray_methods.get_spatial_coverage_attributes(
+        ds
+    )  # Just add spatial/time range as attributes
+    ds = xarray_methods.convert_variables_to_erddap_format(
+        ds
+    )  # Add encoding information to netcdf and convert strings
 
     # Finally save the xarray dataset to a NetCDF file!!!
     #  Make the global attribute variables preceding the original variables.
     variables_order = metadata_variable_list
-    variables_order.extend(['geographic_area'])
+    variables_order.extend(["geographic_area"])
     variables_order.extend(original_variables)
-    if 'variables' in config:
-        variables_order.extend(config['variables'])
+    if "variables" in config:
+        variables_order.extend(config["variables"])
 
     # Save to NetCDF
     ds[variables_order].to_netcdf(output_path)
@@ -146,9 +172,14 @@ def convert_test_files(config):
 
         except Exception as e:
             # Copy problematic files to a subfolder
-            if not os.path.isdir(os.path.join(os.path.dirname(f), 'failed')):
-                os.mkdir(os.path.join(os.path.dirname(f), 'failed'))
-            shutil.copy(f, os.path.join(os.path.dirname(f), 'failed', os.path.split(f)[-1]))
+            if not os.path.isdir(os.path.join(os.path.dirname(f), "failed")):
+                os.mkdir(os.path.join(os.path.dirname(f), "failed"))
+            shutil.copy(
+                f,
+                os.path.join(
+                    os.path.dirname(f), "failed", os.path.split(f)[-1]
+                ),
+            )
 
             print("***** ERROR***", f)
             print(e)
@@ -183,5 +214,3 @@ if __name__ == "__main__":
     )
 
     convert_test_files(config)
-
-
