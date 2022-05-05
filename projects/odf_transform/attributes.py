5780dd17c3b5dc0c709414f9734a85f8ddc0c224
Jessy Barrette <jessy.barrette@hakai.org>
2022-05-05 16:36:58 -0400

    Add program {season} as project for AZMP and Groundfish

--- projects/odf_transform/attributes.py
+++ projects/odf_transform/attributes.py
@@ -253,10 +253,14 @@ def global_attributes_from_header(ds, odf_header):
             cruise_name += [
                 "Spring" if 1 <= ds.attrs["event_start_time"].month <= 7 else "Fall"
             ]
-        if ds.attrs["program"] == "Groundfish":
+        elif ds.attrs["program"] == "Groundfish":
             cruise_name += [
                 "Summer" if 5 <= ds.attrs["event_start_time"].month <= 9 else "Winter"
             ]
+        # Add program {season} to as project for some specific programs
+        if ds.attrs["program"] in ("Atlantic Zone Monitoring Program","Groundfish"):
+            ds.attrs["project"] = cruise_name
+
         cruise_name += [str(ds.attrs["event_start_time"].year)]
         ds.attrs["cruise_name"] = " ".join(cruise_name)
 
