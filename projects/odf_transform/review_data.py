b58df72c6ca4207b6d0ef01c6151600a12c8f917
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2022-10-25 12:42:46 -0400

    drop bad temperature and salinity bad values

---
+++ projects/odf_transform/review_data.py
@@ -0,0 +1,27 @@
+import logging
+
+import numpy as np
+import xarray as xr
+
+logger = logging.getLogger(__name__)
+
+standard_name_range = {
+    "sea_water_temperature": [-20, 100],
+    "sea_water_salinity": [-5, 100],
+}
+
+
+def review_data_range(dataset: xr.Dataset, drop_bad_values: bool = True) -> xr.Dataset:
+    """Review each standard_name variables and drop any values out of the acceptable range.s"""
+    for standard_name, range in standard_name_range.items():
+        for var in dataset.filter_by_attrs(standard_name=standard_name):
+            in_range = (range[0] < dataset[var]) & (dataset[var] < range[1])
+            if any(~in_range) and drop_bad_values:
+                dataset[var] = xr.where(in_range, dataset[var], np.nan)
+                logger.warning(
+                    "Some values in %s is out of range %s %s.",
+                    var,
+                    standard_name_range,
+                    range,
+                )
+    return dataset
