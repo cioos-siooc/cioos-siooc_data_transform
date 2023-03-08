88ad44bf0d400128d3377b630dac5cbc7461a056
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-03-08 15:15:51 -0500

    refactor ios_data_transform_script to leverage logging module, tqdm and argparse python packages

--- projects/ios_data_transform/ios_data_transform_script.py
+++ projects/ios_data_transform/ios_data_transform_script.py
@@ -1,6 +1,10 @@
+import logging
+import logging.config
 import os
 import sys
 import glob
+import argparse
+from tqdm import tqdm
 from multiprocessing import Process
 from time import time
 import cioos_data_transform.IosObsFile as ios
@@ -12,6 +16,11 @@ from write_mctd_ncfile import write_mctd_ncfile
 import cioos_data_transform.utils as cioos_utils
 import subprocess
 
+log_config_path = os.path.join(os.path.dirname(__file__), "log_config.ini")
+logging.config.fileConfig(log_config_path)
+main_logger = logging.getLogger()
+logger = logging.LoggerAdapter(main_logger, {"file": None})
+
 
 def convert_files(config={}, opt="all", ftype=None):
     # path of raw files, path for nc files, and option
@@ -20,7 +29,7 @@ def convert_files(config={}, opt="all", ftype=None):
     # ftype =   'ctd' for CTD profiles
     #           'mctd' for mooring CTDs
     #           'cur' for current meters
-    print("Option, ftype =", opt, ftype)
+    logger.info("Option, option=%s, ftype =%s", opt, ftype)
     in_path = config.get("raw_folder")
     # out_path = config.get("nc_folder")
     # fgeo = config.get("geojson_file")
@@ -37,12 +46,15 @@ def convert_files(config={}, opt="all", ftype=None):
     elif ftype == "cur":
         flist = glob.glob(in_path + "**/*.[Cc][Uu][Rr]", recursive=True)
     else:
-        print("ERROR: Filetype not understood ...")
+        logger.error("ERROR: Filetype not understood ...")
         return None
-    print("Total number of files =", len(flist))
+    logger.info("Total number of files =%s", len(flist))
     # loop through files in list, read the data and write netcdf file if data read is successful
-    for i, fname in enumerate(flist[:]):
+    for fname in tqdm(
+        flist[:], unit="file", desc=f"Convert files {ftype} to netcdf format"
+    ):
         # print('\nProcessing -> {} {}'.format(i, fname))
+        logger.extra["file"] = fname
         p = Process(target=(convert_files_threads), args=(ftype, fname, config))
         p.start()
         p.join()
@@ -53,7 +65,7 @@ def standardize_variable_names(ncfile):
     # input: netcdf file with non-standard variable names
     # output: netcdf file with standard variables added
     # NOTE: netcdf files are overwritten
-    print(f"Adding standard variables:{ncfile}")
+    logger.info(f"Adding standard variables:{ncfile}")
     cioos_utils.add_standard_variables(ncfile)
 
 
@@ -62,7 +74,7 @@ def convert_files_threads(ftype, fname, config={}):
     if cioos_utils.file_mod_time(fname) < -24.0 and opt == "new":
         # print("Not converting file: ", fname)
         return 0
-    print("Processing {} {}".format(ftype, fname))
+    logger.debug("Processing %s %s", ftype, fname)
     # read file based on file type
     if ftype == "ctd":
         fdata = ios.CtdFile(filename=fname, debug=False)
@@ -73,11 +85,11 @@ def convert_files_threads(ftype, fname, config={}):
     elif ftype == "cur":
         fdata = ios.CurFile(filename=fname, debug=False)
     else:
-        print("Filetype not understood!")
+        logger.error("Filetype not understood!")
         sys.exit()
     # if file class was created properly, try to import data
     if fdata.import_data():
-        print("Imported data successfully!")
+        logger.debug("Imported data successfully!")
         fdata.assign_geo_code(config.get("geojson_file"))
         out_path = config.get("nc_folder")
         # now try to write the file...
@@ -120,25 +132,33 @@ def convert_files_threads(ftype, fname, config={}):
 # read inputs if any from the command line
 # first input is 'all' or 'new' for processing all files or just files newer than 24 hours
 # second input is file type and is one of ['ctd','mctd', 'cur', 'bot']
-if len(sys.argv) > 1:
-    opt = sys.argv[1].strip().lower()
-    ftype = sys.argv[2].strip().lower()
-else:  # default option. process all files !
-    opt = "all"
-    ftype = "ctd"
-config = cioos_utils.read_config(f"config_{ftype}.json")
-print("Inputs from config file: ", config)
-start = time()
-flist = convert_files(config=config, opt=opt, ftype=ftype)
-print("Time taken to convert files: {:0.2f}".format(time() - start))
-# if any raw files have been removed, delete corresponding netCDF files
-if flist is not None:
-    print("Checking if any netCDF files should be removed...")
-    ncfilelist = glob.glob(config.get("nc_folder") + "**/*.nc", recursive=True)
-    for i, e in enumerate(
-        cioos_utils.compare_file_list(sub_set=flist, global_set=ncfilelist)
-    ):
-        filename = glob.glob(config.get("nc_folder") + "**/{}.nc")
-        print("deleting file:", e)
-        subprocess.call(["rm", "-f", e])
-print("Total time taken:{:0.2f}".format(time() - start))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="IOStoNetCDF",
+        description="Convert IOS Shell file format to NetCDF format.",
+        epilog="CIOOS (2023)",
+    )
+    parser.add_argument(
+        "opt",
+        default="all",
+        choices=("all", "new"),
+        help="all: processing all files or new: just files newer than 24 hours",
+    )
+    parser.add_argument("ftype", default="ctd", help="IOS file type to convert")
+    args = parser.parse_args()
+    opt, ftype = args.opt, args.ftype
+
+    config = cioos_utils.read_config(f"config_{ftype}.json")
+    logger.debug("Inputs from config file: %s", config)
+    start = time()
+    flist = convert_files(config=config, opt=opt, ftype=ftype)
+    logger.info("Time taken to convert files: %0.02fs", time() - start)
+    # if any raw files have been removed, delete corresponding netCDF files
+    if flist is not None:
+        logger.info("Checking if any netCDF files should be removed...")
+        ncfilelist = glob.glob(config.get("nc_folder") + "**/*.nc", recursive=True)
+        for e in cioos_utils.compare_file_list(sub_set=flist, global_set=ncfilelist):
+            filename = glob.glob(config.get("nc_folder") + "**/{}.nc")
+            logger.info("deleting file: %s", e)
+            subprocess.call(["rm", "-f", e])
+    logger.info("Total time taken %0.2fs", time() - start)
