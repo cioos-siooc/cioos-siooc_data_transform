d1ad451838f817bd2e74066186117247a1d13b0e
Jessy Barrette <30420025+JessyBarrette@users.noreply.github.com>
2023-04-12 17:15:43 -0400

    ignore ANE file conversion for now

--- projects/ios_data_transform/pac-dev1_dev_cronjob.sh
+++ projects/ios_data_transform/pac-dev1_dev_cronjob.sh
@@ -12,7 +12,7 @@ echo $(which python)
 python ios_data_transform_script.py all drf -i /data/ios_raw_files/drifter_data/ -o /data/erddap_data/IOS_DRF_Data/
 python ios_data_transform_script.py all tob -i /data/ios_raw_files/cruise_data/ -o /data/erddap_data/IOS_TOB_Data/
 python ios_data_transform_script.py all ubc -i /data/ios_raw_files/ubc/ -o /data/erddap_data/IOS_UBC_Data/
-python ios_data_transform_script.py all ane -i /data/ios_raw_files/weather_data/ -o /data/erddap_data/IOS_ANE_Data/
+# python ios_data_transform_script.py all ane -i /data/ios_raw_files/weather_data/ -o /data/erddap_data/IOS_ANE_Data/
 
 python ios_data_transform_script.py all ctd -i /data/ios_raw_files/cruise_data/ -o /data/erddap_data/IOS_CTD_profiles_updated/
 python ios_data_transform_script.py all mctd -i /data/ios_raw_files/mooring_data/ -o /data/erddap_data/IOS_CTD_moorings_updated/
